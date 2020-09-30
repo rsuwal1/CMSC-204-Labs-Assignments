2a300fbf926e700fa797c1e6fbc855e84d328e7f
Rabindra Raj Suwal <60520833+rsuwal1@users.noreply.github.com>
2020-09-30 12:28:50 -0400

    Add files via upload

---
+++ Recursion Lab/ArraySum.java
@@ -0,0 +1,8 @@
+public class ArraySum {
+	public int sumOfArray (Integer[] a,int index) {
+		if (index < 0) {
+		      return 0;
+		    }
+		    return sumOfArray(a, index - 1) + a[index];
+		  }
+}
